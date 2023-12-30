aa5e3d88201d89405a187a2278bff774da8f991b
iamantoon <oneloveeeeeeant@gmail.com>
2023-12-30 15:58:14 +0200

    Error handling

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app/layout/App.tsx'
 import './app/layout/styles.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -8,7 +7,6 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes.tsx';
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
