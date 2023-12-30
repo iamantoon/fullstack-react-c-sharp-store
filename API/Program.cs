ff2e1f1d60a4786f7687a8f404608d2aa3c9a6e1
iamantoon <oneloveeeeeeant@gmail.com>
2023-12-30 06:50:02 +0200

    Exception Middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using API.Data;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,9 @@ builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseMiddleware<ExceptionMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
